3ba37bf27b8e31ffee47171f9b3990e480296b0f
Carlos Giron <carlosegiron92@gmail.com>
2026-02-26 19:40:16 -0500

    init commit for project main branch

--- Projects/04-FastAS7341/Arduino/9G/9G.ino
+++ Projects/04-FastAS7341/Arduino/9G/9G.ino
@@ -1,3 +1,5 @@
+// Board name: Seeed XIAO nRF52840                               FQBN: Seeeduino:nrf52:xiaonRF52840
+
 /* ###################     0. PD, LED Driver library     ################### */
 
 #include <Wire.h>
@@ -11,15 +13,35 @@
 #define NUM_TLC59711 1
 
 // TLC5947 wiring pins (board-specific naming; these must match your MCU pin map).
-#define DATA_PIN  10
+#define DATA_PIN 10
 #define CLOCK_PIN 8
-bool serialDebug = true;  // device will serial output all the data
+bool serialDebug = true; // device will serial output all the data
 
 // ---- Serial debug helpers ----
-#define DBG_BEGIN(baud) do { if (serialDebug) Serial.begin(baud); } while (0)
-#define DBG_PRINT(x)    do { if (serialDebug) Serial.print(x); } while (0)
-#define DBG_PRINTLN(x)  do { if (serialDebug) Serial.println(x); } while (0)
-#define DBG_WRITE(x)    do { if (serialDebug) Serial.write(x); } while (0)
+#define DBG_BEGIN(baud)         \
+    do                          \
+    {                           \
+        if (serialDebug)        \
+            Serial.begin(baud); \
+    } while (0)
+#define DBG_PRINT(x)         \
+    do                       \
+    {                        \
+        if (serialDebug)     \
+            Serial.print(x); \
+    } while (0)
+#define DBG_PRINTLN(x)         \
+    do                         \
+    {                          \
+        if (serialDebug)       \
+            Serial.println(x); \
+    } while (0)
+#define DBG_WRITE(x)         \
+    do                       \
+    {                        \
+        if (serialDebug)     \
+            Serial.write(x); \
+    } while (0)
 // Create TLC5947 instance. Constructor order is (numBoards, clk, data, latch).
 Adafruit_TLC59711 tlc = Adafruit_TLC59711(NUM_TLC59711, CLOCK_PIN, DATA_PIN);
 
@@ -62,12 +84,10 @@ const int negativeAmbient = 0;
 
 // Activation order for LEDs. If you want a custom sweep pattern, reorder this list.
 const int ledOrder[numGroups] = {
-    8, 0, 1, 2, 3, 4, 5, 6, 7
-};
+    8, 0, 1, 2, 3, 4, 5, 6, 7};
 
 int ledBrightness[ledNum] = {
-    3000, 5000, 8000, 8000, 8000, 8000, 8000, 8000, 1200
-};
+    3000, 5000, 8000, 8000, 8000, 8000, 8000, 8000, 1200};
 
 // Index and LED wavelength map:
 
@@ -99,30 +119,39 @@ String combineData(uint16_t readings[], int led, int pwm);
   - Ensures the TLC5947 is initialized and drives all used LED channels to 0 PWM.
   - This is called during setup and whenever USB is detected.
 */
-void allLedsOff() {
+void allLedsOff()
+{
     tlc.begin();
-    for (int i = 0; i < ledNum; i++) {
+    for (int i = 0; i < ledNum; i++)
+    {
         tlc.setPWM(i, 0);
     }
     tlc.write();
 }
 
-void setup() {
+void setup()
+{
     allLedsOff();
-    if (!as7341.begin()) {
+    if (!as7341.begin())
+    {
         DBG_PRINTLN("Could not find AS7341");
-        while (1) { delay(findAS7341Delay); }
+        while (1)
+        {
+            delay(findAS7341Delay);
+        }
     }
     // Configure AS7341 integration and gain.
     as7341.setATIME(atime);
     as7341.setASTEP(astep);
-    as7341.setGain(gain);    
+    as7341.setGain(gain);
     tlc.begin();
     delay(startDelay);
 }
 
-void loop() {
-    for (int g = 0; g < numGroups; g++) {
+void loop()
+{
+    for (int g = 0; g < numGroups; g++)
+    {
         // Select the LED index for this iteration based on ledOrder.
         int led = ledOrder[g];
         /*
@@ -136,21 +165,22 @@ void loop() {
         tlc.write();
         delay(ledOnDelay);
 
-        if (!as7341.readAllChannels()) {
-        DBG_PRINTLN("Error reading all channels!");
-        return;
+        if (!as7341.readAllChannels())
+        {
+            DBG_PRINTLN("Error reading all channels!");
+            return;
         }
 
-        readings[0]  = (int)as7341.getChannel(AS7341_CHANNEL_415nm_F1);
-        readings[1]  = (int)as7341.getChannel(AS7341_CHANNEL_445nm_F2);
-        readings[2]  = (int)as7341.getChannel(AS7341_CHANNEL_480nm_F3);
-        readings[3]  = (int)as7341.getChannel(AS7341_CHANNEL_515nm_F4);
-        readings[4]  = (int)as7341.getChannel(AS7341_CHANNEL_555nm_F5);
-        readings[5]  = (int)as7341.getChannel(AS7341_CHANNEL_590nm_F6);
-        readings[6]  = (int)as7341.getChannel(AS7341_CHANNEL_630nm_F7);
-        readings[7]  = (int)as7341.getChannel(AS7341_CHANNEL_680nm_F8);
-        readings[8]  = (int)as7341.getChannel(AS7341_CHANNEL_NIR);
-        readings[9]  = (int)as7341.getChannel(AS7341_CHANNEL_CLEAR);
+        readings[0] = (int)as7341.getChannel(AS7341_CHANNEL_415nm_F1);
+        readings[1] = (int)as7341.getChannel(AS7341_CHANNEL_445nm_F2);
+        readings[2] = (int)as7341.getChannel(AS7341_CHANNEL_480nm_F3);
+        readings[3] = (int)as7341.getChannel(AS7341_CHANNEL_515nm_F4);
+        readings[4] = (int)as7341.getChannel(AS7341_CHANNEL_555nm_F5);
+        readings[5] = (int)as7341.getChannel(AS7341_CHANNEL_590nm_F6);
+        readings[6] = (int)as7341.getChannel(AS7341_CHANNEL_630nm_F7);
+        readings[7] = (int)as7341.getChannel(AS7341_CHANNEL_680nm_F8);
+        readings[8] = (int)as7341.getChannel(AS7341_CHANNEL_NIR);
+        readings[9] = (int)as7341.getChannel(AS7341_CHANNEL_CLEAR);
 
         String pdData = combineData(readings, led, ledBrightness[led]);
         DBG_PRINTLN(pdData);
@@ -162,31 +192,32 @@ void loop() {
 
         // Count how many LEDs we’ve processed since the last ambient update.
         ledLoopCounter++;
-
     }
 }
 
-String combineData(uint16_t readings[], int led, int pwm) {
+String combineData(uint16_t readings[], int led, int pwm)
+{
     String pdData = "";
 
     // Time since boot (HH-MM-SS.mmm)
     unsigned long ms = millis();
     unsigned long totalSeconds = ms / 1000UL;
 
-    unsigned int hours   = totalSeconds / 3600UL;
+    unsigned int hours = totalSeconds / 3600UL;
     unsigned int minutes = (totalSeconds % 3600UL) / 60UL;
     unsigned int seconds = totalSeconds % 60UL;
-    unsigned int msec    = ms % 1000UL;
+    unsigned int msec = ms % 1000UL;
 
     char timeBuf[16];
     snprintf(timeBuf, sizeof(timeBuf), "%02u-%02u-%02u.%03u",
-            hours, minutes, seconds, msec);
+             hours, minutes, seconds, msec);
 
     // Prefix timestamp
     pdData += String(timeBuf) + ",";
 
     // Append all PD channels
-    for (int i = 0; i < totalPDNum; i++) {
+    for (int i = 0; i < totalPDNum; i++)
+    {
         pdData += String(readings[i]) + ",";
     }
 
