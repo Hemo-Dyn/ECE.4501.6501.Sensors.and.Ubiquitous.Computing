4598d5fad37a6041e01b63d6dd515d6a02bd6806
Carlos Giron <carlosegiron92@gmail.com>
2026-03-24 12:41:32 -0400

    added tarek code

--- Projects/04-FastAS7341/Arduino/9G/FastPD.cpp
+++ Projects/04-FastAS7341/Arduino/9G/FastPD.cpp
@@ -1,5 +1,6 @@
 #include "Arduino.h"
 #include "inc/FastPD.hpp"
+#include "Adafruit_AS7341.h"
 #include <string.h>
 
 // Index and LED wavelength map:
@@ -15,6 +16,23 @@ FastPD::FastPD() : Adafruit_AS7341()
     setATIME(A_TIME);
     setASTEP(A_STEP);
     setGain((as7341_gain_t)AS7341_GAIN_256X);
+}
+
+bool FastPD::loadSMUXCustomTable(const uint8_t table[20]) {
+
+    // 1. Disable spectral measurement
+    enableSpectralMeasurement(false);
+
+    // 2. Write all 20 SMUX bytes to RAM FIRST
+    for (uint8_t i = 0; i < 20; i++) {
+        writeRegister(0x00 + i, table[i]);   // SMUX RAM starts at 0x00
+    }
+
+    // 3. NOW set SMUX command to WRITE (copies RAM -> SMUX chain)
+    setSMUXCommand(AS7341_SMUX_CMD_WRITE);
+
+    // 4. Commit SMUX config (sets SMUXEN bit to execute the command)
+    return enableSMUX();
 }
 
 void FastPD::update_readings()
